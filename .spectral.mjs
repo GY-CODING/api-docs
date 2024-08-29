113dfa494670cbf705b92df10789e1c7eb036fa0
ToxYc <gycoding05@gmail.com>
2024-08-29 18:04:00 +0200

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NzExMQ/spectral.js?branch_slug=master&token=f52fcbb8-1910-47a9-b5f6-046353a1c989";
+export default { extends: ruleset };
